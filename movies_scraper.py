f89799e6ad84a41d75ee4bddce6c8db51515e9b2
moviehub4ubot <122563459+moviehub4ubot@users.noreply.github.com>
2023-05-25 23:20:10 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -10,6 +10,7 @@ def search_movies(query):
     movies_list = []
     movies_details = {}
     website = BeautifulSoup(requests.get(f"https://84.46.254.230/?s={query.replace(' ', '+')}").text, "html.parser")
+    website = BeautifulSoup(requests.get(f"https://ww4.mkvcinemas.lat/?s={query.replace(' ', '+')}").text, "html.parser")
     movies = website.find_all("a", {'class': 'ml-mask jt'})
     for movie in movies:
         if movie:
