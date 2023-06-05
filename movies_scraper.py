0e41ceee331b0cf7011bd183b28f852ddebb2703
moviehub4ubot <122563459+moviehub4ubot@users.noreply.github.com>
2023-06-05 21:24:36 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -4,39 +4,47 @@ from bs4 import BeautifulSoup
 
 url_list = {}
 api_key ="6dadfdffd0cec854aa06cbae97da7c0678a0eccf"
+websites = [
+    {'name': 'Website 1', 'url': 'https://84.46.254.230/'},
+    {'name': 'Website 2', 'url': 'https://example2.com/movies?search='}
+]
 
 
 def search_movies(query):
     movies_list = []
     movies_details = {}
-    website = BeautifulSoup(requests.get(f"https://84.46.254.230/?s={query.replace(' ', '+')}").text, "html.parser")
-    movies = website.find_all("a", {'class': 'ml-mask jt'})
-    for movie in movies:
-        if movie:
-            movies_details["id"] = f"link{movies.index(movie)}"
-            movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
-            url_list[movies_details["id"]] = movie['href']
-        movies_list.append(movies_details)
-        movies_details = {}
+    for website in websites:
+        website_name = website['name']
+        website_url = website['url']
+        search_url = f"{website_url}{query.replace(' ', '+')}"
+        website_content = BeautifulSoup(requests.get(search_url).text, "html.parser")
+        movies = website_content.find_all("a", {'class': 'ml-mask jt'})
+        for movie in movies:
+            if movie:
+                movies_details["id"] = f"{website_name}-link{movies.index(movie)}"
+                movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
+                url_list[movies_details["id"]] = movie['href']
+            movies_list.append(movies_details)
+            movies_details = {}
     return movies_list
 
 
 def get_movie(query):
     movie_details = {}
-    movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
-    if movie_page_link:
-        title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
-        movie_details["title"] = title
-        img = movie_page_link.find("div", {'class': 'mvic-thumb'})['data-bg']
-        movie_details["img"] = img
-        links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
-        final_links = {}
-        for i in links:
-            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
-            response = requests.get(url)
-            link = response.json()
-            final_links[f"{i.text}"] = link['shortenedUrl']
-        movie_details["links"] = final_links
+    movie_link = url_list.get(query)
+    if movie_link:
+        movie_page_content = BeautifulSoup(requests.get(movie_link).text, "html.parser")
+        if movie_page_content:
+            title = movie_page_content.find("div", {'class': 'mvic-desc'}).h3.text
+            movie_details["title"] = title
+            img = movie_page_content.find("div", {'class': 'mvic-thumb'})['data-bg']
+            movie_details["img"] = img
+            links = movie_page_content.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
+            final_links = {}
+            for link in links:
+                final_links[link.text] = link['href']
+            movie_details["links"] = final_links
     return movie_details
+
 
 
